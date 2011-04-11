b1b2d2586dd8a4438de8c5273d44beed104ab382
Andrew Burgess <abombm1@gmail.com>
2011-04-10 22:55:18 -0400

    Completed Activity shows correct layout

--- src/com/deceptacle/spartacus/CompletedActivity.java
+++ src/com/deceptacle/spartacus/CompletedActivity.java
@@ -9,6 +9,7 @@ public class CompletedActivity extends Activity {
 	@Override
 	public void onCreate(Bundle savedInstance) {
 		super.onCreate(savedInstance);
+		setContentView(R.layout.completed);
 		
 		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 	}
